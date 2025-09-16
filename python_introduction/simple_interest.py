4afc6d7524dd861b3f7e78b5fc64224e0dce318c
naomi wilson <naomiwilson699@gmail.com>
2025-09-16 15:35:58 -0700

    Add simple_interest with principal, rate and time

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+principal = 1000   # the money you invest
+rate = 0.05        # 5% interest rate as a decimal
+time = 3           # number of years
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
+
+
