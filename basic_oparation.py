d6ce2b0e74b97ddbe9c3ee78df55fe30c1b78916
naomi wilson <naomiwilson699@gmail.com>
2025-09-16 14:39:26 -0700

    Add basic_operations.py with sum, difference, and product

---
+++ basic_oparation.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition_result = number1 + number2
+substruction_results = number1 - number2
+multiplication_results = number1 * number2 
+
+print("addition of", number1, "and", number2, "is", addition_result)
+print("substruction of", number1, "and", number2, "is", substruction_results)
+print("product of", number1, "and", number2, "is", multiplication_results)
